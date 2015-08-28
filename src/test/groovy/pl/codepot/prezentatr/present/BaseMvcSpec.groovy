ef5fd770dd21be4835fddc2f2b1dadd895894b93
Mateusz Rasiński <mrn@touk.pl>
2015-08-28 14:27:00 +0200

    Working - tests failing

--- src/test/groovy/pl/codepot/prezentatr/present/BaseMvcSpec.groovy
+++ src/test/groovy/pl/codepot/prezentatr/present/BaseMvcSpec.groovy
@@ -1,8 +1,6 @@
 package pl.codepot.prezentatr.present
 
 import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc
-import pl.codepot.prezentatr.feed.FeedController
-import pl.codepot.prezentatr.feed.FeedRepository
 import spock.lang.Specification
 
 class BaseMvcSpec extends Specification {
