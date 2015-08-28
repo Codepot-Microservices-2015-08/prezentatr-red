7d78687989b5138a5af23c03cd1f860f2d2f25b1
Mateusz Rasiński <mateusz.rasinski@gmail.com>
2015-08-28 17:58:03 +0200

    Fix mocks in tests

--- src/test/groovy/pl/codepot/prezentatr/present/BaseMvcSpec.groovy
+++ src/test/groovy/pl/codepot/prezentatr/present/BaseMvcSpec.groovy
@@ -1,13 +1,21 @@
 package pl.codepot.prezentatr.present
-
 import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc
+import pl.codepot.prezentatr.dto.Ingredient
+import pl.codepot.prezentatr.dto.IngredientType
+import pl.codepot.prezentatr.dto.Order
+import pl.codepot.prezentatr.service.IngredientService
 import spock.lang.Specification
 
 class BaseMvcSpec extends Specification {
 
     def setup() {
         setupMocks()
-        RestAssuredMockMvc.standaloneSetup(new FeedController(), new PresentController())
+        IngredientService ingredientService = Mock(IngredientService)
+        ingredientService.getIngredients(_) >> new Order(ingredients: [new Ingredient(IngredientType.MALT, 200),
+                                                                       new Ingredient(IngredientType.WATER, 200),
+                                                                       new Ingredient(IngredientType.HOP, 200),
+                                                                       new Ingredient(IngredientType.YIEST, 200)])
+        RestAssuredMockMvc.standaloneSetup(new FeedController(), new PresentController(ingredientService))
     }
 
     void setupMocks() {
